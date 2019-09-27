f44861f9350b377dca0d85f10f6d4cb3acf5edd3
Per Engström <per@precisit.com>
2019-09-27 11:46:41 +0200

    Initial commit

---
+++ ClientApp/src/index.tsx
@@ -0,0 +1,22 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+  import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
+const rootElement = document.getElementById('root');
+
+if (!baseUrl) {
+  throw new Error(`Base URL was not found.`);
+}
+
+ReactDOM.render(
+  <BrowserRouter basename={baseUrl}>
+    <App />
+  </BrowserRouter>,
+  rootElement);
+
+registerServiceWorker();
+
